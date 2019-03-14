9fc0e0a9b2295e8e915cc96e289a66e412c4b0ae
yixiangD <doggydeng@gmail.com>
2019-03-14 10:55:20 -0400

    twitter scraper with automatically scroll down feature for specific amount of times

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -3,28 +3,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 '''
     Reference:
-
-    Twitter scraper tutorial with Python: Requests, BeautifulSoup, and Selenium — Part 1 and Part 2
-    by Randy Daw-Ran Liou
+    Scrapping Infinitely scrolling websites Using Pthon Selenium. August 22, 2015
+    By Raghavendra
 
     Link:
-    https://medium.com/@dawran6/twitter-scraper-tutorial-with-python-requests-beautifulsoup-and-selenium-part-1-8e76d62ffd68
-    https://medium.com/@dawran6/twitter-scraper-tutorial-with-python-requests-beautifulsoup-and-selenium-part-2-b38d849b07fe
+    http://raghavendra990.github.io/2015/08/22/scrapping-using-selenium.html
 '''
 def main():
     browser = webdriver.Firefox()
     query = 'kungfupanda'
     url = 'https://twitter.com/search?l=&q=%23' + query + '&src=typd'
     browser.get(url)
-    time.sleep(1)
+    # scroll down for 100 times and sleep for 3 seconds between scrolls to 
+    # allow data loading
+    nscroll = 100
 
-    body = browser.find_element_by_tag_name('body')
-
-    nscroll = 20
-    for _ in range(nscroll):
-        body.send_keys(Keys.PAGE_DOWN)
-        time.sleep(0.5)
+    for i in range(nscroll):
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+    
     tweets = browser.find_elements_by_class_name('tweet-text')
+    for tweet in tweets:
+        print(tweet.text)
     print(len(tweets))
 
 if __name__ == "__main__":
