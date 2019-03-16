48ca229d694546828609cf40c61a09a9b1cd2f30
yixiangD <doggydeng@gmail.com>
2019-03-15 20:18:24 -0400

    adding counting of retweets and likes

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -16,16 +16,38 @@ def main():
     browser.get(url)
     # scroll down for 100 times and sleep for 3 seconds between scrolls to 
     # allow data loading
-    nscroll = 100
+    nscroll = 2
 
     for i in range(nscroll):
         browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(3)
-    
-    tweets = browser.find_elements_by_class_name('tweet-text')
-    for tweet in tweets:
-        print(tweet.text)
-    print(len(tweets))
+
+    count = browser.find_elements_by_class_name('stream-item-footer')
+    # just analyze the top 1000 tweets
+
+    ntrunc = 1000
+    if len(count) > ntrunc:
+        count = count[:ntrunc]
+    summ = []
+    for item in count:
+        text = item.text
+        array = text.split()
+        res = []
+        if array[-1] == "Like":
+            res += ["Like", 0]
+        else:
+            res += ["Like", int(array[-1])]
+        if array[1] == "Retweet" and array[2] == "Like":
+            res += ["Retweet", 0]
+        elif array[1] == "Retweet" and array[2] != "Like":
+            res += ["Retweet", int(array[2])]
+        elif array[2] == "Retweet" and array[3] == "Like":
+            res += ["Retweet", 0]
+        elif array[2] == "Retweet" and array[3] != "Like":
+            res += ["Retweet", int(array[3])]
+        summ.append(res)
+    for item in summ:
+        print(item)
 
 if __name__ == "__main__":
     main()
