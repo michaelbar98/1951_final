125d0f61d3b9490a6d029e1f4fe9dfce2d5b6941
yixiangD <doggydeng@gmail.com>
2019-03-30 23:30:32 -0400

    support query by movie title hashtag and close browser automatically, output summary of data

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -1,34 +1,38 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-'''
-    Reference:
-    Scrapping Infinitely scrolling websites Using Pthon Selenium. August 22, 2015
-    By Raghavendra
+import selenium as se
+import sys
 
-    Link:
-    http://raghavendra990.github.io/2015/08/22/scrapping-using-selenium.html
-'''
-def main():
+order_dict = {"K": 1000.0, "M": 1e6, "B": 1e9}
+
+def interpret_str(s):
+    try:
+        return int(s)
+    except:
+        order = s[-1]
+        value = float(s[:-1])
+        return int(value * order_dict[order])
+
+def query_movie(query):
     browser = webdriver.Firefox()
-    query = 'kungfupanda'
     url = 'https://twitter.com/search?l=&q=%23' + query + '&src=typd'
     browser.get(url)
     # scroll down for 100 times and sleep for 3 seconds between scrolls to 
     # allow data loading
-    nscroll = 2
+    nscroll = 3
 
     for i in range(nscroll):
         browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(3)
 
     count = browser.find_elements_by_class_name('stream-item-footer')
-    # just analyze the top 1000 tweets
-
     ntrunc = 1000
     if len(count) > ntrunc:
         count = count[:ntrunc]
     summ = []
+
+    table = {"Title": query, "Count": len(count), "Like": 0, "Retweet": 0}
     for item in count:
         text = item.text
         array = text.split()
@@ -36,18 +40,31 @@ def main():
         if array[-1] == "Like":
             res += ["Like", 0]
         else:
-            res += ["Like", int(array[-1])]
+            likes = interpret_str(array[-1])
+            res += ["Like", likes]
+            table["Like"] += likes
         if array[1] == "Retweet" and array[2] == "Like":
             res += ["Retweet", 0]
         elif array[1] == "Retweet" and array[2] != "Like":
-            res += ["Retweet", int(array[2])]
+            retweets = interpret_str(array[2])
+            res += ["Retweet", retweets]
+            table["Retweet"] += retweets
         elif array[2] == "Retweet" and array[3] == "Like":
             res += ["Retweet", 0]
         elif array[2] == "Retweet" and array[3] != "Like":
-            res += ["Retweet", int(array[3])]
+            retweets = interpret_str(array[3])
+            res += ["Retweet", retweets]
+            table["Retweet"] += retweets
         summ.append(res)
-    for item in summ:
-        print(item)
+    browser.stop_client()
+    browser.close()
+    return table
+
+def main():
+    query = 'kungfupanda'
+    table = query_movie(query)
+    print(table)
 
 if __name__ == "__main__":
     main()
+
